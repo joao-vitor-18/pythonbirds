df1df5cda6a527616a3e54c9424eca43c12eca90
joao-vitor-18 <leal.vitor@rocketmail.com>
2021-07-06 12:56:18 -0300

    Criados atributos de instancia

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,16 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=28):
+        self.idade=idade
+        self.nome=nome
+
     def cumprimentar(self):
         return f'Olá {id(p)}'
 
 if __name__=='__main__':
-    p = Pessoa()
+    p = Pessoa('Pedro')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    p.nome = 'João'
+    print(p.nome)
+    print(p.idade)
